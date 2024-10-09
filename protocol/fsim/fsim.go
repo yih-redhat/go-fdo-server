235e4e848f90a486ff0919dcd20808a5ea5d694c
Shrikant Temburwar <shrikant.temburwar@intel.com>
2024-10-09 10:30:30 +0530

    Remove local protocol code
    Signed-off-by: Shrikant Temburwar <shrikant.temburwar@intel.com>

--- protocol/fsim/fsim.go
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: (C) 2024 Intel Corporation
-// SPDX-License-Identifier: Apache 2.0
-
-// Package fsim implements common FSIM modules defined in
-// https://github.com/fido-alliance/fdo-sim/tree/main/fsim-repository as well
-// as plugin modules as defined in plugin/README.md.
-package fsim
